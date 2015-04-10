fccd82eacbb50a54b297f1ebb7d00a474fa95495
Stephane Nicoll <snicoll@pivotal.io>
2015-04-10 21:38:46 +0200

    Step1: add simple HornetQ demo
    Demonstrate a simple use of the HornetQ auto-configuration. An external
    broker running on localhost is required.

--- hornetq-sample-app/src/main/java/demo/DemoApplication.java
+++ hornetq-sample-app/src/main/java/demo/DemoApplication.java
@@ -1,13 +1,56 @@
 package demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Service
+	static class MessageProducer implements CommandLineRunner {
+
+		private static final Logger logger = LoggerFactory.getLogger(MessageProducer.class);
+
+		private final JmsTemplate jmsTemplate;
+
+		@Autowired
+		public MessageProducer(JmsTemplate jmsTemplate) {
+			this.jmsTemplate = jmsTemplate;
+		}
+
+		@Override
+		public void run(String... strings) throws Exception {
+			process("Hello World");
+		}
+
+		public void process(String msg) {
+			logger.info("============= Sending " + msg);
+			this.jmsTemplate.convertAndSend("testQueue", msg);
+		}
+	}
+
+	@Component
+	static class MessageHandler {
+
+		private static final Logger logger = LoggerFactory.getLogger(MessageHandler.class);
+
+		@JmsListener(destination = "testQueue")
+		public void processMsg(String msg) {
+			logger.info("============= Received " + msg);
+		}
 	}
 
 }
